6cb66c3608269a8af8a0d2d564a5edeaabd95a49
Divyansh Pratap Singh <1729divyansh@gmail.com>
2022-09-10 10:57:30 +0530

    created new file

---
+++ programs/read-n-values-in-array-display-elements-in-reverse-order.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("enter number of elements you will enter");
+    int n;
+    scanf("%d" , &n);
+    int arr[n];
+    
+    // taking input  from the user
+    printf("enter  elements in array");
+    for(int i=0;i<n;i++){
+        scanf("%d" , &arr[i]);
+    }
+    printf("original array \n");
+    for(int k=0;k<n;k++){
+        printf("%d \t" , arr[k]);
+    }
+    printf("\n reversed  array \n");
+    
+    //  displaying the array in reverse order
+    for(int j=n-1 ;j>=0;j--){
+        printf("%d \t" , arr[j]);    
+    }
+}
+
